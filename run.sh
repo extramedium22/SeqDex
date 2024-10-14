497a0dbea215b596cc0dcf5d86b4db94fa65a038
SkyCaptain <extramedium22@163.com>
2024-10-14 10:18:56 +0800

    get realman arm ready.

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 python train_rlgames.py --task=[BlockAssemblySearch / BlockAssemblyOrient / BlockAssemblyGraspSim / BlockAssemblyInsertSim]   --num_envs=1
 python train_rlgames.py --task=BlockAssemblySearch --num_envs=1
 python train_rlgames.py --task=InspireBlockAssemblySearch --num_envs=1
+python train_rlgames.py --task=InspireGraspBlock --num_envs=2
 
 
 python -m debugpy --listen 34567 --wait-for-client train_rlgames.py --task=InspireBlockAssemblySearch --num_envs=1
